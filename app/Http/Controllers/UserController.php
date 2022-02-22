1120a50f35a1ebbc9d324421e3c11b9f1989ac7a
Rodrigo Gomes Weich <rodrigoweich@hotmail.com>
2022-02-21 22:39:03 -0300

    Create functions in User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\Response;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\User\StoreRequest;
+use App\Http\Requests\User\UpdateRequest;
 
 class UserController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $user = User::create($request->validated());
+        return new UserResource($user);
+    }
+
+    public function show($id)
+    {
+        return new UserResource(User::findOrFail($id));
+    }
+
+    public function update(UpdateRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->validated());
+        return new UserResource($user);
+    }
+
+    public function destroy($id)
+    {
+        User::findOrFail($id)->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
 }
