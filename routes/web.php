a5d19cc2d1f4b924bef84897663e8776d657fa7b
Rodrigo Gomes Weich <rodrigoweich@hotmail.com>
2022-02-21 22:37:14 -0300

    Added User routers

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('users', 'UserController@index');
+    $router->post('users', 'UserController@store');
+    $router->get('users/{id}', 'UserController@show');
+    $router->put('users/{id}', 'UserController@update');
+    $router->delete('users/{id}', 'UserController@destroy');
+});
